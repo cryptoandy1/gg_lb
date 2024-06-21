456dcfb9770554a8c95b4458078556d204c66e18
cryptoandy1 <a.r.bcwall@gmail.com>
2024-06-22 01:32:57 +0300

    ipoker fetch fixed 2

--- fetchIp.js
+++ fetchIp.js
@@ -1,5 +1,5 @@
 const fetchIPLB = async () => {
-  const res = await fetch('https://storo08-server.onrender.com/current')
+  const res = await fetch('https://storo08.up.railway.app/current')
   const json = await res.json()
   return json
 }
